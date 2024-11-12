1d97caca6b42d9ee4959f7adfb56e2158b76a975
HBhawe <harshalb97@outlook.com>
2024-11-12 09:39:56 +0100

    Initial commit

---
+++ 3.2 HTTP Requests/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+const port = 3000;
+
+app.use(morgan("combined"));
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>About Me</h1><p>My name is Harshal</p>");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("<h1>Contact Me</h1><p>Phone: +fkjgbdfklgjq374636</p>");
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
