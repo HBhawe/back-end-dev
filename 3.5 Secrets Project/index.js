a7167edad9dcba693908b8d8fd63f90eaa0b2160
HBhawe <harshalb97@outlook.com>
2024-11-12 17:01:52 +0100

    Secrets mini-project

--- 3.5 Secrets Project/index.js
+++ 3.5 Secrets Project/index.js
@@ -1,3 +1,35 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+
+import express, { response } from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const sendResponse = function (req) {
+  //   pw is ILoveProgramming
+  const password = req.body.password;
+  return !(!password || password !== "ILoveProgramming");
+};
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", (req, res) => {
+  if (sendResponse(req)) {
+    res.sendFile(__dirname + "/public/secret.html");
+  } else res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
