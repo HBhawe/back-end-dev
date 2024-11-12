b2ccf60aa3e9149e09ba6d598df8b41b8576cca4
HBhawe <harshalb97@outlook.com>
2024-11-12 13:40:01 +0100

    Sending HTML markup as a response

--- 3.1 Express Server/index.js
+++ 3.1 Express Server/index.js
@@ -3,6 +3,12 @@ const app = express();
 const port = 3000;
 // const host = "127.0.0.1";
 
+const html = `<!DOCTYPE html>
+                        <body> 
+                          <p>This is a test</p></body>`;
+app.get("/", (req, res) => {
+  res.send(html);
+});
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
 });
