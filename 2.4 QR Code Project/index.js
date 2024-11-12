7963924d7294e09f36093d4ad742c03f98993b1f
HBhawe <harshalb97@outlook.com>
2024-11-12 11:30:13 +0100

    QR code generator

--- 2.4 QR Code Project/index.js
+++ 2.4 QR Code Project/index.js
@@ -9,27 +9,27 @@ import qr from "qr-image";
 import fs from "fs";
 
 inquirer
-    .prompt([
-        {
-            type: "input",
-            message: "Enter a URL",
-            name: "URL"
-        }
-    ])
-    .then((answers) => {
-        var url = answers.URL;
-        var qr_svg = qr.image(url);
-        qr_svg.pipe(fs.createWriteStream('qr_img.png'));
+  .prompt([
+    {
+      type: "input",
+      message: "Enter a URL",
+      name: "URL",
+    },
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    const qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
 
-        fs.writeFile('URL.txt', url, (err) => {
-            if (err) throw err;
-            console.log('The file has been saved!');
-        });
-    })
-    .catch((error) => {
-        if (error.isTtyError) {
-            // Prompt couldn't be rendered in the current environment
-        } else {
-            // Something else went wrong
-        }
+    fs.writeFile("URL.txt", url, (err) => {
+      if (err) throw err;
+      console.log("The file has been saved!");
     });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
