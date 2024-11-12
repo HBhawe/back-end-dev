1bf8fa0839f6f8cd88646647f1d805d87c81adc8
HBhawe <harshalb97@outlook.com>
2024-11-12 10:26:12 +0100

    Using npm modules

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,16 @@
+// COMMON JS IMPORT
+// const generateName = require("sillyname");
+// let sillyName = generateName();
+
+// console.log(`My name is ${sillyName}`);
+
+// MODULE IMPORT
+// import generateName from "sillyname";
+//
+// let name = generateName();
+// console.log(`My name is ${name}`);
+
+// using the superheroes package
+import { randomSuperhero } from "superheroes";
+
+console.log(`I am ${randomSuperhero()}!`);
