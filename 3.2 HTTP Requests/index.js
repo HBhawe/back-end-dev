0336bb59394a14ea75f0cd74911d5bfee024e504
HBhawe <harshalb97@outlook.com>
2025-08-07 17:23:40 +0200

    Added nav element

--- 3.2 HTTP Requests/index.js
+++ 3.2 HTTP Requests/index.js
@@ -8,22 +8,18 @@ const port = 3000;
 
 app.use(morgan("combined"));
 
+let navigator = `<nav><a href="/"><button>Home</button></a><a href="/about"><button>About</button></a><a href="/contact"><button>Contact</button></a></nav>`;
+
 app.get("/", (req, res) => {
-  res.send(
-    `<h2>Hello</h2><a href="/about"><button>About</button></a><a href="/contact"><button>Contact</button></a>`,
-  );
+  res.send(`${navigator}<h2>Hello</h2>`);
 });
 
 app.get("/about", (req, res) => {
-  res.send(
-    '<h1>About Me</h1><p>My name is Harshal</p><a href="/"><button>Home</button></a>',
-  );
+  res.send(`${navigator}<h1>About Me</h1><p>My name is Harshal</p>`);
 });
 
 app.get("/contact", (req, res) => {
-  res.send(
-    '<h1>Contact Me</h1><p>Phone: +fkjgbdfklgjq374636</p><a href="/"><button>Home</button></a>',
-  );
+  res.send(`${navigator}<h1>Contact Me</h1><p>Phone: +fkjgbdfklgjq374636</p>`);
 });
 
 app.listen(port, () => {
