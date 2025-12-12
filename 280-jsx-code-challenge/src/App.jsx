d236c87aa0c283da272df3bb40eb29a7f24af9c6
HBhawe <harshalb97@outlook.com>
2025-12-12 16:31:45 +0100

    Intro to JSX

---
+++ 280-jsx-code-challenge/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hi!</h1>
+      <ul>
+        <li>Element 1</li>
+        <li>Element 2</li>
+        <li>Element 3</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
