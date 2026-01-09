bd2cb42129fa4473a756ddd74ec15454690fddfb
HBhawe <harshalb97@outlook.com>
2026-01-09 14:53:34 +0100

    React components

---
+++ 286-react-components-start/src/components/App.jsx
@@ -0,0 +1,14 @@
+import "../App.css";
+import { Heading } from "./Heading.jsx";
+import { List } from "./List.jsx";
+
+function App() {
+  return (
+    <>
+      <Heading />
+      <List />
+    </>
+  );
+}
+
+export default App;
