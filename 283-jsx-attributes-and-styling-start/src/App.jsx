830196536508b17401bc202bd9e61a7c8ded2639
HBhawe <harshalb97@outlook.com>
2026-01-09 14:37:48 +0100

    JSX styling

---
+++ 283-jsx-attributes-and-styling-start/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Favourite Foods</h1>
+      <ul>
+        <li>Bacon</li>
+        <li>Jamon</li>
+        <li>Noodles</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
