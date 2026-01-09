174ff57b6bd43f23930a53231009b74f88b7f304
HBhawe <harshalb97@outlook.com>
2026-01-09 16:57:51 +0100

    Extracting map logic to function

--- 295-react-props/src/App.jsx
+++ 295-react-props/src/App.jsx
@@ -3,21 +3,23 @@ import { Card } from "./Card.jsx";
 import contacts from "./Contacts.js";
 
 function App() {
+  const createCard = function (contact) {
+    return (
+      <Card
+        name={contact.name}
+        imgSrc={contact.imgURL}
+        phone={contact.phone}
+        email={contact.email}
+        key={contact.name}
+      />
+    );
+  };
+
   return (
     <>
       <div>
         <h1 className="heading">My Contacts</h1>
-        {contacts.map((contact) => {
-          return (
-            <Card
-              name={contact.name}
-              imgSrc={contact.imgURL}
-              phone={contact.phone}
-              email={contact.email}
-              key={contact.name}
-            />
-          );
-        })}
+        {contacts.map(createCard)}
       </div>
     </>
   );
