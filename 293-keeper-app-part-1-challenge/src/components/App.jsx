682669b0a0da85c720de1ff3a00697ac9f162004
HBhawe <harshalb97@outlook.com>
2026-01-09 15:45:12 +0100

    Keeper 1

---
+++ 293-keeper-app-part-1-challenge/src/components/App.jsx
@@ -0,0 +1,16 @@
+import "../App.css";
+import { Footer } from "./Footer.jsx";
+import { Header } from "./Header.jsx";
+import { Note } from "./Note.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Note />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
